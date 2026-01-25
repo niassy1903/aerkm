bd6042a38c6b930cccc1ad509da6477b3b35b2c4
niassy1903 <niassy.lamine10@gmail.com>
2026-01-25 01:09:51 +0000

    edit s13

--- plateforme-aerkm---keur-massar/pages/auth/Login.tsx
+++ plateforme-aerkm---keur-massar/pages/auth/Login.tsx
@@ -22,7 +22,7 @@ const Login: React.FC = () => {
   const navigate = useNavigate();
 
   // URL de l'API en production
-  const API_URL = 'http://localhost:5000/api';
+  const API_URL = 'https://aerkm.onrender.com/api';
 
   // Détection du token dans l'URL
   useEffect(() => {
